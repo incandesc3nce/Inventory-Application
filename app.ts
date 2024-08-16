0dec448790b97b686400bb57a017ea0a3ae27e7d
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-16 21:54:42 +0700

    feat: use public assets

--- app.ts
+++ app.ts
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
