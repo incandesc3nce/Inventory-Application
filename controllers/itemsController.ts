24c61afa48bab81e6ace72a2bdf1b136d566aaaa
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-16 22:43:32 +0700

    feat: modify item to include genre name

--- controllers/itemsController.ts
+++ controllers/itemsController.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from 'express';
 import { getItems } from '../db/queries/itemsQueries';
+import { getGenreById } from '../db/queries/genresQueries';
 import Item from '../types/Item';
+import Genre from '../types/Genre';
 
 const getItemRows = async () => {
   const items: Item[] = await getItems();
+  items.map(async (item: Item) => {
+    const genre: Genre = await getGenreById(item.genre_id);
+    item.genre = genre.name;
+  })
   return items;
 }
 
--- types/Item.ts
+++ types/Item.ts
@@ -4,6 +4,8 @@ type Item = {
   description: string;
   category_id: number;
   genre_id: number;
+  genre: string;
+  img_url: string;
 };
 
 export default Item;
