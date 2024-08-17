46c8adf5e4d0e57756f5afd1d4a96bcc6d2d1b0c
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-17 23:37:22 +0700

    style: prettier

--- controllers/categoriesController.ts
+++ controllers/categoriesController.ts
@@ -1,13 +1,16 @@
-import { Request, Response } from "express";
-import { getCategories } from "../db/queries/categoriesQueries";
-import Category from "../types/Category";
+import { Request, Response } from 'express';
+import { getCategories } from '../db/queries/categoriesQueries';
+import Category from '../types/Category';
 
 const getCategoryRows = async () => {
   const categories: Category[] = await getCategories();
   return categories;
-}
+};
 
 export const categoriesController = async (req: Request, res: Response) => {
   const categories: Category[] = await getCategoryRows();
-  res.render("categories", { title: "Categories List", categories: categories });
-}
+  res.render('categories', {
+    title: 'Categories List',
+    categories: categories,
+  });
+};
