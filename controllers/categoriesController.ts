1acd0e7aa166e165aef5d351ca4f0e006835271b
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-17 22:16:04 +0700

    feat: create controller for categories

---
+++ controllers/categoriesController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { getCategories } from "../db/queries/categoriesQueries";
+import Category from "../types/Category";
+
+const getCategoryRows = async () => {
+  const categories: Category[] = await getCategories();
+  return categories;
+}
+
+export const categoriesController = async (req: Request, res: Response) => {
+  const categories: Category[] = await getCategoryRows();
+  res.render("categories", { title: "Categories List", categories: categories });
+}
