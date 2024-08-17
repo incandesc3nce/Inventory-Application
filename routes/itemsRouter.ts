798dd60194cf76182760abddfa7dab43590c2acf
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-17 21:43:56 +0700

    feat: setup an individual item route

--- routes/itemsRouter.ts
+++ routes/itemsRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { itemsController } from '../controllers/itemsController';
+import { itemController } from '../controllers/itemController';
 
 const itemsRouter: Router = Router();
 
 itemsRouter.get('/', itemsController);
+itemsRouter.get('/:id', itemController);
 
 export default itemsRouter;
