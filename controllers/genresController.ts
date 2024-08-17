0683ac4374d9e51bc6f73e90dd6d5b0b2dce8815
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-17 23:37:45 +0700

    feat: create controller for genres

---
+++ controllers/genresController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { getGenres } from '../db/queries/genresQueries';
+import Genre from '../types/Genre';
+
+const getGenreRows = async () => {
+  const genres: Genre[] = await getGenres();
+  return genres;
+};
+
+export const genresController = async (req: Request, res: Response) => {
+  const genres: Genre[] = await getGenreRows();
+  res.render('genres', { title: 'Genres List', genres: genres });
+};
