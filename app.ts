03d6ae8829c9ebbf75b781ea99d9d40d690645b3
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-17 22:19:44 +0700

    feat: add categories route to app

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import indexRouter from './routes/indexRouter';
 import itemsRouter from './routes/itemsRouter';
+import categoriesRouter from './routes/categoriesRouter';
 import path from 'path';
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 app.use('/items', itemsRouter);
+app.use('/categories', categoriesRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
