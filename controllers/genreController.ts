f292061ea07f8d31165c9399b02555930893ffcb
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-17 23:51:09 +0700

    feat: create controller for getting items by genre

---
+++ controllers/genreController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { getItemsByGenre } from '../db/queries/itemsQueries';
+import { getGenreById } from '../db/queries/genresQueries';
+import Item from '../types/Item';
+import Genre from '../types/Genre';
+
+const getItemsByGenreRows = async (id: number) => {
+  const genre: Genre = await getGenreById(id);
+  if (!genre) {
+    throw new Error(`404: Couldn't find genre with id ${id}`);
+  }
+  const items: Item[] = await getItemsByGenre(id);
+  await Promise.all(
+    items.map(async (item: Item) => {
+      item.genre = genre.name;
+    })
+  );
+  return items;
+};
+
+export const genreController = async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id);
+  try {
+    const items: Item[] = await getItemsByGenreRows(id);
+    res.render('items', { title: 'Genre', items: items });
+  } catch (err) {
+    console.log(err);
+    res.render('404');
+  }
+};
--- routes/genresRouter.ts
+++ routes/genresRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { genresController } from '../controllers/genresController';
+import { genreController } from '../controllers/genreController';
 
 const genresRouter: Router = Router();
 
 genresRouter.get('/', genresController);
+genresRouter.get('/:id', genreController);
 
 export default genresRouter;
