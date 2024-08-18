3ab923c0ba62b227fcddb82b771a38c6fcd17bdf
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-19 00:07:30 +0700

    feat: extract form action into a page property

--- controllers/items/editItemController.ts
+++ controllers/items/editItemController.ts
@@ -24,6 +24,7 @@ export const editItemController = async (req: Request, res: Response) => {
   res.render("itemForm", {
     title: "Edit Item",
     item: item,
+    action: `/items/${id}?_method=PUT`,
     categories: categories,
     genres: genres,
   });
