9b20c26eb0e3e9b86cdef83a207773a007462009
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-18 22:46:25 +0700

    feat: add item edit route

--- routes/itemsRouter.ts
+++ routes/itemsRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { itemsController } from '../controllers/items/itemsController';
 import { itemController } from '../controllers/items/itemController';
+import { editItemController, updateItemController } from '../controllers/items/editItemController';
 
 const itemsRouter: Router = Router();
 
 itemsRouter.get('/', itemsController);
 itemsRouter.get('/:id', itemController);
+itemsRouter.get('/:id/edit', editItemController);
+itemsRouter.post('/:id/edit', updateItemController);
 
 export default itemsRouter;
