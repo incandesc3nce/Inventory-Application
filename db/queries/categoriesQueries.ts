9c053cd75fee51fcb5b87976763051bc5f275a06
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-18 22:41:47 +0700

    feat: add queries for finding rows by name

--- db/queries/categoriesQueries.ts
+++ db/queries/categoriesQueries.ts
@@ -6,23 +6,53 @@ const getCategories = async () => {
 };
 
 const getCategoryById = async (id: number) => {
-  const { rows } = await pool.query('SELECT * FROM categories WHERE id = $1;', [id]);
+  const { rows } = await pool.query('SELECT * FROM categories WHERE id = $1;', [
+    id,
+  ]);
+  return rows[0];
+};
+
+const getCategoryByName = async (name: string) => {
+  const { rows } = await pool.query(
+    'SELECT * FROM categories WHERE name = $1;',
+    [name]
+  );
   return rows[0];
 };
 
 const createCategory = async (name: string, description: string) => {
-  const { rows } = await pool.query('INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *;', [name, description]);
+  const { rows } = await pool.query(
+    'INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *;',
+    [name, description]
+  );
   return rows[0];
 };
 
-const updateCategory = async (id: number, name: string, description: string) => {
-  const { rows } = await pool.query('UPDATE categories SET name = $1, description = $2 WHERE id = $3 RETURNING *;', [name, description, id]);
+const updateCategory = async (
+  id: number,
+  name: string,
+  description: string
+) => {
+  const { rows } = await pool.query(
+    'UPDATE categories SET name = $1, description = $2 WHERE id = $3 RETURNING *;',
+    [name, description, id]
+  );
   return rows[0];
 };
 
 const deleteCategory = async (id: number) => {
-  const { rows } = await pool.query('DELETE FROM categories WHERE id = $1 RETURNING *;', [id]);
+  const { rows } = await pool.query(
+    'DELETE FROM categories WHERE id = $1 RETURNING *;',
+    [id]
+  );
   return rows[0];
 };
 
-export { getCategories, getCategoryById, createCategory, updateCategory, deleteCategory };
+export {
+  getCategories,
+  getCategoryById,
+  getCategoryByName,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+};
--- db/queries/genresQueries.ts
+++ db/queries/genresQueries.ts
@@ -6,23 +6,48 @@ const getGenres = async () => {
 };
 
 const getGenreById = async (id: number) => {
-  const { rows } = await pool.query('SELECT * FROM genres WHERE id = $1;', [id]);
+  const { rows } = await pool.query('SELECT * FROM genres WHERE id = $1;', [
+    id,
+  ]);
+  return rows[0];
+};
+
+const getGenreByName = async (name: string) => {
+  const { rows } = await pool.query('SELECT * FROM genres WHERE name = $1;', [
+    name,
+  ]);
   return rows[0];
 };
 
 const createGenre = async (name: string) => {
-  const { rows } = await pool.query('INSERT INTO genres (name) VALUES ($1) RETURNING *;', [name]);
+  const { rows } = await pool.query(
+    'INSERT INTO genres (name) VALUES ($1) RETURNING *;',
+    [name]
+  );
   return rows[0];
-}
+};
 
 const updateGenre = async (id: number, name: string) => {
-  const { rows } = await pool.query('UPDATE genres SET name = $1 WHERE id = $2 RETURNING *;', [name, id]);
+  const { rows } = await pool.query(
+    'UPDATE genres SET name = $1 WHERE id = $2 RETURNING *;',
+    [name, id]
+  );
   return rows[0];
 };
 
 const deleteGenre = async (id: number) => {
-  const { rows } = await pool.query('DELETE FROM genres WHERE id = $1 RETURNING *;', [id]);
+  const { rows } = await pool.query(
+    'DELETE FROM genres WHERE id = $1 RETURNING *;',
+    [id]
+  );
   return rows[0];
 };
 
-export { getGenres, getGenreById, createGenre, updateGenre, deleteGenre };
+export {
+  getGenres,
+  getGenreById,
+  getGenreByName,
+  createGenre,
+  updateGenre,
+  deleteGenre,
+};
