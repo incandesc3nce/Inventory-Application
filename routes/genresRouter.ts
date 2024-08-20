9f4f2099e2239b0957c816865c81959099cfa31e
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-20 14:30:03 +0700

    feat: add routes for genres actions

--- routes/genresRouter.ts
+++ routes/genresRouter.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { genresController } from '../controllers/genres/genresController';
 import { genreController } from '../controllers/genres/genreController';
+import { deleteGenreController } from '../controllers/genres/deleteGenreController';
+import { addGenreController, createGenreController } from '../controllers/genres/addGenreController';
+import { editGenreController, updateGenreController } from '../controllers/genres/editGenreController';
 
 const genresRouter: Router = Router();
 
 genresRouter.get('/', genresController);
+genresRouter.get('/new', addGenreController);
+genresRouter.post('/new', createGenreController);
 genresRouter.get('/:id', genreController);
+genresRouter.get('/:id/edit', editGenreController);
+genresRouter.post('/:id/edit', updateGenreController);
+genresRouter.get('/:id/delete', deleteGenreController);
 
 export default genresRouter;
