16b8a5cffa665b897fa1689c1fb309893d8ffc03
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-20 13:46:47 +0700

    feat: add routes for category actions

--- routes/categoriesRouter.ts
+++ routes/categoriesRouter.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { categoriesController } from '../controllers/categories/categoriesController';
 import { categoryController } from '../controllers/categories/categoryController';
+import { addCategoryController, createCategoryController } from '../controllers/categories/addCategoryController';
+import { editCategoryController, updateCategoryController } from '../controllers/categories/editCategoryController';
+import { deleteCategoryController } from '../controllers/categories/deleteCategoryController';
 
 const categoriesRouter: Router = Router();
 
 categoriesRouter.get('/', categoriesController);
+categoriesRouter.get('/new', addCategoryController);
+categoriesRouter.post('/new', createCategoryController);
 categoriesRouter.get('/:id', categoryController);
+categoriesRouter.get('/:id/edit', editCategoryController);
+categoriesRouter.post('/:id/edit', updateCategoryController);
+categoriesRouter.get('/:id/delete', deleteCategoryController);
 
 export default categoriesRouter;
